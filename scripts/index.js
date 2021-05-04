d599124d5c86226d89857b9615f5f1505d27e50c
JOSEPH HARRIS <jharris99@my.bcit.ca>
2021-05-04 14:05:43 -0700

    added some script files and folder

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAPes5NkunBodm_wtBsW8b-78t8jNfhsgE",
+    authDomain: "trailmates-657f0.firebaseapp.com",
+    projectId: "trailmates-657f0",
+    storageBucket: "trailmates-657f0.appspot.com",
+    messagingSenderId: "877707794753",
+    appId: "1:877707794753:web:a9ee3826e467041bed69c0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
