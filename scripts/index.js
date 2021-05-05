2a0841130ce76738eb0db54908373db83e70745a
Jason Banh <jbanh2@gmail.com>
2021-05-05 12:25:43 -0700

    Pair Programming with Joe. Completed Skeleton Nav and Sidebar

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,16 @@
 
+function openClose() {
+  let style = getComputedStyle(document.getElementById("mySidenav"))
+  let width = style.width;
+  switch (width) {
+    case "250px":
+      document.getElementById("mySidenav").style.width = "0";
+      break;
+    case "0px":
+      document.getElementById("mySidenav").style.width = "250px";
+      break;
+  }
+}
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
