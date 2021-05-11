31a977dfaffb71b5289220d2edbe1f2debf16cb8
simron-adiwal <sadiwal@my.bcit.ca>
2021-05-11 15:12:32 -0700

    Connected page to Firebase such that content updates dynamically

---
+++ scripts/hikedetail.js
@@ -0,0 +1,9 @@
+// Populates list with items from firestore
+// function hikeQuery() {
+//     firebase.auth().onAuthStateChanged(function (user) {
+
+//         db.collection('hikeDetails').doc(user.uid)
+//             .collection('Garibaldi')
+//             .get()
+//     })
+// };
