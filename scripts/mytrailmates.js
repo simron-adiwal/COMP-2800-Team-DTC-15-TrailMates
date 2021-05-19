60aa5090b4620019eeba2e8a57b60806d4f67e14
Dongwan Kim <kim93dong@gmail.com>
2021-05-19 09:38:20 -0700

    Write function for using firestore: pair programming with Joeseph

--- scripts/mytrailmates.js
+++ scripts/mytrailmates.js
@@ -1,14 +1,24 @@
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         //userId = user.uid;
-        db.collection("users").doc(user.uid).get().then(function(user) {
+        db.collection("users").doc(user.uid).get().then(function (user) {
             loadUserFriends(user.data());
         });
     }
+    else console.log("Authentication failed.");
 });
 
 function loadUserFriends(user) {
     console.log(user.friendslist)
+
+    let friendList = user.friendslist;
+    friendList.forEach((friend) => {
+        db.collection("users").doc(friend).get().then(function (frienduser) {
+            let frienddata = frienduser.data();
+            console.log(frienddata.name);
+        });
+    });
+
 };
 
 
